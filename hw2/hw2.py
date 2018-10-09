ccb770c853df554f6b94ed3341b6dfaed4ee9603
Maren Cosens <mcosens@ucsd.edu>
2018-10-09 15:08:04 -0700

    added part 3 to hw2.py

--- hw2/hw2.py
+++ hw2/hw2.py
@@ -6,6 +6,7 @@
     using Python 2.7
 '''
 import numpy as np
+import matplotlib.pyplot as pl
 
 ##Define variables given in problem statement
 D=100 #cloud depth [pc]
@@ -44,4 +45,28 @@ I_D=intensity_one_freq(3.24e-21, 10, 100, 1, 100)
 print("""Part 2:
       sigma=3.24e-21 cm^-2, I(0)=10, S_nu=100, n=1cm^-3, D=100pc:
       I(s=D) = {0}
-      """.format(I_D)
+      """.format(I_D))
+
+##Part 3:
+#define function to convert from FWHM to variance
+def Gamma2sigma(Gamma):
+    return Gamma/ ( np.sqrt(2 * np.log(2)) * 2 )
+#define function to create cross section as function of frequency
+def cross_section(freq_range, fwhm, height):    #takes frequency range and peak frequency as inputs
+    x=freq_range
+    x0=x[len(x)/2]
+    return height * np.exp( - 0.5*((x-x0)/Gamma2sigma(fwhm))**2 )
+
+#generate plots of cross-section for peak values in part 1
+frequency=np.arange(10,100,1)
+sigma_nu=[cross_section(frequency, 1, sigma[i]) for i in range(len(sigma))]
+pl.figure("Cross-sections as function of frequency")
+pl.plot(frequency, sigma_nu[0], 'r--', label='$\\sigma_{\\nu,0}=3.24\\times10^{-24} cm^{-2}$')
+pl.plot(frequency, sigma_nu[1], 'b--', label='$\\sigma_{\\nu,0}=3.24\\times10^{-21} cm^{-2}$')
+pl.plot(frequency, sigma_nu[2], 'k--', label='$\\sigma_{\\nu,0}=3.24\\times10^{-18} cm^{-2}$')
+pl.xlabel('Frequency [arbitrary units]', fontsize=15)
+pl.ylabel('Cross section [$cm^{-2}$]', fontsize=15)
+pl.title('$\\sigma_{\\nu}: \\nu_0=55, FWHM=1$')
+pl.yscale('log')
+pl.legend(loc='upper right', numpoints=1, ncol=1, fontsize=8, edgecolor='k')   #create legend
+#pl.show()
