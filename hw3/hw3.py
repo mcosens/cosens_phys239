e328029c122e8ff684e286a81c82706809158043
Maren Cosens <mcosens@ucsd.edu>
2018-11-06 10:11:24 -0800

    added an attempt at part 4 that doesn't look quite right yet

--- hw3/hw3.py
+++ hw3/hw3.py
@@ -7,12 +7,13 @@
     '''
 import numpy as np
 import matplotlib.pyplot as pl
-#from mpl_toolkits import mplot3d
+import scipy.integrate as int
 
-##Define initial variables
+##Part 1: Define initial variables
 #constants
 eps_0=8.8542*10**(-12)    #[C^2/N/m^2]; permittivity of free space
 a_0=5.29177e-11 #[m]; Bohr radius
+c=3e9   #[m/s] speed of light
 #electron parameters
 x_0=-300*a_0 #[m] initial x and y positions are 1000*Bohr radius (R=sqrt(x^2+y^2)~283)
 y_0=400*a_0   #[m]; electron initially up and left from ion
@@ -38,7 +39,7 @@ v_y[0]=v_0_y    #populate first cell with initial velocity
 a_x=np.zeros(len(t))    #array of acceleration in x
 a_y=np.zeros(len(t))    #array of acceleration in y
 
-##calclate force between electron and ion at each time intercal to fill in position, velocity, and acceleration arrays
+##Part 2: calclate force between electron and ion at each time intercal to fill in position, velocity, and acceleration arrays
 for i in range(1,len(t)):
     R=np.sqrt(x[i-1]**2+y[i-1]**2)  #radius
     theta=np.arctan(x[i-1]/y[i-1])  #angle between x and y positions
@@ -52,17 +53,15 @@ for i in range(1,len(t)):
     y[i]=y[i-1]+ (np.mean((v_y[i],v_y[i-1]))*del_t)
 
 
-##Plot results of interaction
+##Part 3: Plot results of interaction
 pl.figure('Part 3')
-#pl.title('postion of electron')
+#position
 pl.subplot(1,3,1)
 pl.plot(x/a_0,y/a_0, '-b', label='electron path')
 pl.xlabel('x position [$a_0^{-1}$]')
 pl.ylabel('y position [$a_0^{-1}$]')
 pl.legend()
-#pl.show()
-
-
+#velocity
 pl.subplot(1,3,2)
 pl.title('$x_0=300a_0, y_0=400a_0, v_{x,0}=10^5 m \ s^{-1}$')
 pl.plot(t, v_x, '-b', label='velocity in x')
@@ -70,12 +69,49 @@ pl.plot(t, v_y, '-k', label='velocity in y')
 pl.xlabel('time [s]')
 pl.ylabel('velocity [$m \ s^{-1}$]')
 pl.legend()
-
+#acceleration
 pl.subplot(1,3,3)
-#pl.title('electron acceleration')
 pl.plot(t, a_x, '-b', label='acceleration in x')
 pl.plot(t, a_y, '-k', label='accelleration in y')
 pl.xlabel('time [s]')
 pl.ylabel('acceleration [$m \ s^{-2}$]')
 pl.legend()
+#pl.show()
+
+##Part 4: Fourier transform acceleration to get power spectrum as a function of frequency
+#array of frequencies
+w=np.arange(0,1600,1600.0/float(len(t)))   #angular frequency
+nu=w/(2*np.pi)    #frequency
+#convert to polar coordinates
+R=np.sqrt(x**2 +y**2)
+theta=np.arctan(x/y)
+#acceleration in polar
+a_r=(a_x*np.sin(theta) + a_y*np.cos(theta))
+a_theta=(-1*a_x*np.cos(theta)+a_y*np.sin(theta))
+#dipole moment
+#w2d_r=(Q_e/(2*np.pi()*R))*int.trapz(
+#Fourier transform each acceleration array
+a_r_w=np.fft.fft(a_r)   #a_r(w)
+a_theta_w=np.fft.fft(a_theta)   #a_theta(w)
+#dW/(dOmege*Domega)
+dW_dO_dw_r = ((w**2 * np.sin(theta)**2) *Q_e /(2*(np.pi**2)*c**3)) *a_r_w**2
+dW_dO_dw_theta = ((w**2 * np.sin(theta)**2) *Q_e /(2*(np.pi**2)*c**3)) *a_theta_w**2
+#dW/(dOmega*domega)
+dW_dO_dnu_r = ((2*nu**2 * np.sin(theta)**2) *Q_e /(c**3)) *a_r_w**2
+dW_dO_dnu_theta = ((2*nu**2 * np.sin(theta)**2) *Q_e /(c**3)) *a_theta_w**2
+
+pl.figure('power spectrum')
+#radial direction
+pl.subplot(2,1,1)
+pl.plot(nu,dW_dO_dnu_r)
+pl.yscale('log')
+pl.xlabel('frequency [Hz]')
+pl.ylabel('Power/steradian')
+#theta direction
+pl.subplot(2,1,2)
+pl.plot(nu, dW_dO_dnu_theta)
+pl.yscale('log')
+pl.xlabel('frequency [Hz]')
+pl.ylabel('Power/steradian')
 pl.show()
+
